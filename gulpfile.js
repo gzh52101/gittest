0c465f89d4dc14540d35ce6c9ef87606394c59b1
aaron <jeroo@qq.com>
2021-04-10 14:17:34 +0800

    添加sass监听

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,11 @@
 const gulp = require('gulp');
 
-gulp.task('default',()=>{
+gulp.task('default',(done)=>{
     gulp.src('xxx')
     .pipe(gulp.dest('./dist'))
+
+    gulp.watch('./src/sass/*.scss',()=>{
+
+    })
+    done();
 })
